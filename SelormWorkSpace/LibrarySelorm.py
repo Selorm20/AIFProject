c0be9e99d2ee6849b12935c37339bb5215424a55
Selorm20 <selormapoh61@gmail.com>
2023-12-20 15:53:14 +0100

    Update LibrarySelorm.py

--- SelormWorkSpace/LibrarySelorm.py
+++ SelormWorkSpace/LibrarySelorm.py
@@ -1,3 +1,29 @@
-class ClassA:
+#Create a class of character actions and define them
+class CharacterActions:
     def __init__(self, Int):
         self.Int = Int
+        
+   def actions_from_path(start: Tuple[int, int], path: List[Tuple[int, int]]) -> List[int]:
+    action_map = {
+        "N": 0,
+        "E": 1,
+        "S": 2,
+        "W": 3
+    }
+    actions = []
+    x_s, y_s = start
+    for (x, y) in path:
+        if x_s == x:
+            if y_s > y:
+                actions.append(action_map["W"])
+            else: actions.append(action_map["E"])
+        elif y_s == y:
+            if x_s > x:
+                actions.append(action_map["N"])
+            else: actions.append(action_map["S"])
+        else:
+            raise Exception("x and y can't change at the same time. oblique moves not allowed!")
+        x_s = x
+        y_s = y
+    
+    return actions
