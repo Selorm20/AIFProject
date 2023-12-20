7e5d0f4dfa371bc2fe92eb200af2114909882b02
Riccardo369 <r.marcaccio@studenti.unipi.it>
2023-12-20 18:38:41 +0100

    Finish implementation A* and BFS algorithm search + testing

--- RiccardoWorkSpace/LibraryRiccardo.py
+++ RiccardoWorkSpace/LibraryRiccardo.py
@@ -1,92 +1,115 @@
-class AstarPathSearch:
-    def __init__(self, GetNearPointsFunction):
-        self.__GetNearPointsFunction = GetNearPointsFunction
+class __PathSearch:
+    def __init__(self, GetNeighbourPointsFunction):
+        
+        self._VisitedElements = [] 
+        self._GetNeighbourPointsFunction = lambda x: list(filter(lambda i: i not in self._VisitedElements, GetNeighbourPointsFunction(x)))
     
-    def GetNearPointsFunction(self):
-        return self.__GetNearPointsFunction
+    def CalculatePath(self, StartPoint, FinishPoint):
+        return NotImplemented
+    
+    def GetMinimumPointPath(self, Points):
+        MinimumPoint, r = Points[0], 0
+        
+        for i in range(len(Points)):
+            if(Points[i].GetCount() < MinimumPoint.GetCount()): MinimumPoint, r = Points[i], i
+            
+        return MinimumPoint, r
+    
+    class _SearchNode:
+        def __init__(self, Element, PreviousNode):
+            self.__PreviousNode = PreviousNode
+            self.__Element = Element
+            self.__Count = 0 if PreviousNode == None else PreviousNode.GetCount()+1
+                
+        def GetPreviousNode(self):
+            return self.__PreviousNode
+            
+        def GetElement(self):
+            return self.__Element
+            
+        def GetCount(self):
+            return self.__Count
+            
+        def GetPath(self):
+            Result = []
+            Step = self
+            
+            while(Step != None):
+                Result.insert(0, Step.GetElement())
+                Step = Step.GetPreviousNode()
+                
+            return Result
+        
+        def __del__(self):
+            return NotImplemented
+
+class AstarPathSearch(__PathSearch):
+    def __init__(self, GetNeighbourPointsFunction):
+        super().__init__(GetNeighbourPointsFunction)
     
     def CalculatePath(self, StartPoint, FinishPoint):
         
-        SearchLeafs = [__SearchNode(StartPoint, None)]
+        SearchLeafs = [self._SearchNode(StartPoint, None)]
+        self._VisitedElements.clear()
         
         #While there is also 1 leaf to consider
         while(len(SearchLeafs) > 0):
             
             #Find index of minimum element
-            MinimumPoint = SearchLeafs[0]
+            PointToAnalyze, r = self.GetMinimumPointPath(SearchLeafs)
+                    
+            self._VisitedElements.append(PointToAnalyze.GetElement())
             
-            r = 0
-            for i in range(len(SearchLeafs)):
-                if(SearchLeafs[i].GetCount() < MinimumPoint.GetCount()):
-                    MinimumPoint, r = SearchLeafs[i], i
+            #Delete minimum point previously explored
+            del SearchLeafs[r]
                     
             #Calculate Neighbour points of minimum point
-            NearPoints = list(map(lambda x: __SearchNode(x, MinimumPoint),  self.__GetNearPointsFunction(MinimumPoint.GetElement())))
+            NearPoints = list(map(lambda x: self._SearchNode(x, PointToAnalyze),  self._GetNeighbourPointsFunction(PointToAnalyze.GetElement())))
             
             #Check if finish point is found
             for i in NearPoints:
                 if(i.GetElement() == FinishPoint): return i.GetPath()
                 
             #Concatenate new points to list of leaf to explore    
-            SearchLeafs += NearPoints
-            
-            #Delete minimum point previously explored
-            del SearchLeafs[r]  
+            SearchLeafs += NearPoints            
             
         #In that case, there is no path from start point to finish point 
         return None
 
-class BFSPathSearch:
-    def __init__(self, GetNearPointsFunction):
-        self.__GetNearPointsFunction = GetNearPointsFunction
-    
-    def GetNearPointsFunction(self):
-        return self.__GetNearPointsFunction
+class BFSPathSearch(__PathSearch):
+    def __init__(self, GetNeighbourPointsFunction):
+        super().__init__(GetNeighbourPointsFunction)
     
     def CalculatePath(self, StartPoint, FinishPoint):
         
-        SearchLeafs = [__SearchNode(StartPoint, None)]
+        SearchLeafs = [self._SearchNode(StartPoint, None)]
+        self._VisitedElements.clear()
+        
+        Result = []
         
         #While there is also 1 leaf to consider
         while(len(SearchLeafs) > 0):
             
             #Find index of minimum element
             MinimumPoint = SearchLeafs[0]
-                    
+          
+            self._VisitedElements.append(MinimumPoint.GetElement())
+            
+            ElementSearchLeafs = list(map(lambda x: x.GetElement(), SearchLeafs))
+
             #Calculate Neighbour points of minimum point
-            NearPoints = list(map(lambda x: __SearchNode(x, MinimumPoint),  self.__GetNearPointsFunction(MinimumPoint.GetElement())))
+            NearPoints = list(filter(lambda x: x not in ElementSearchLeafs or x == FinishPoint, self._GetNeighbourPointsFunction(MinimumPoint.GetElement())))
+            NearPoints = list(map(lambda x: self._SearchNode(x, MinimumPoint), NearPoints))
             
             #Check if finish point is found
             for i in NearPoints:
-                if(i.GetElement() == FinishPoint): return i.GetPath()
+                if(i.GetElement() == FinishPoint): Result.append(i.GetPath())
+                   
+            #Delete minimum point previously explored
+            del SearchLeafs[0]
                 
             #Concatenate new points to list of leaf to explore    
-            SearchLeafs += NearPoints
-            
-            #Delete minimum point previously explored
-            del SearchLeafs[0]  
+            SearchLeafs += NearPoints         
             
         #In that case, there is no path from start point to finish point 
-        return None
-            
-class __SearchNode:
-    def __init__(self, Element, PreviousNode):
-        self.__PreviousNode = PreviousNode
-        self.__Element = Element
-        self.__Count = 0 if PreviousNode == None else PreviousNode.GetCount()+1
-            
-    def GetPreviousNode(self):
-        return self.__PreviousNode
-        
-    def GetElement(self):
-        return self.__Element
-        
-    def GetCount(self):
-        return self.__Count
-        
-    def GetPath(self):
-        if(self.GetPreviousNode() == None): return self.GetElement()
-        return [self.GetPreviousNode().GetPath()] + [self.GetElement()]
-    
-    def __del__(self):
-        return NotImplemented
+        return Result
