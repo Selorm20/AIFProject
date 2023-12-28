a5b656a7f1522307ffe2be50095d7a31966b8797
Riccardo369 <r.marcaccio@studenti.unipi.it>
2023-12-28 10:46:10 +0100

    Level generator inserted

---
+++ SelormWorkSpace/MovingLibrary.py
@@ -0,0 +1,11 @@
+class Moving:
+    def __init__(self, env):
+        self.__env = env
+        
+    #Do next position to enviroment
+    #return True if move is done
+    #return False if move is not done after call this method
+    def Move(self, ActualPosition, NextPosition):
+        return True
+    
+    
