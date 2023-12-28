d672c7139303568c400c8c301a87c02bdd660ad4
i.stracca <i.stracca@studenti.unipi.it>
2023-12-28 22:33:19 +0100

    Added functions to get various locations

--- JacopoWorkSpace/LibraryJacopo.py
+++ JacopoWorkSpace/LibraryJacopo.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Tuple
 
 def remove_black_border(image):
     rows = np.any(image != [0, 0, 0], axis=2).any(axis=1)
@@ -7,3 +8,19 @@ def remove_black_border(image):
     image_without_borders = image[rows][:, cols, :]
 
     return image_without_borders
+
+def get_player_location(game_map: np.ndarray, symbol : str = "@") -> Tuple[int, int]:
+    x, y = np.where(game_map == ord(symbol))
+    return (x[0], y[0])
+
+def get_target_location(game_map: np.ndarray, symbol : str = ">") -> Tuple[int, int]:
+    x, y = np.where(game_map == ord(symbol))
+    return (x[0], y[0])
+
+def get_monsters_location(game_map: np.ndarray, symbol : str = "d"):
+    x, y = np.where(game_map == ord(symbol))
+    arr_returned = []
+    for i in range(len(x)):
+        position = (x[i],y[i])
+        arr_returned.append(position)
+    return arr_returned
