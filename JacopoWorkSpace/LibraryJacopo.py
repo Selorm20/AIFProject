94db58df260776c5d44692c34ae28aff9eeab8a9
i.stracca <i.stracca@studenti.unipi.it>
2023-12-28 19:08:14 +0100

    First map

--- JacopoWorkSpace/LibraryJacopo.py
+++ JacopoWorkSpace/LibraryJacopo.py
@@ -1,3 +1,9 @@
-class ClassA:
-    def __init__(self, Int):
-        self.Int = Int
+import numpy as np
+
+def remove_black_border(image):
+    rows = np.any(image != [0, 0, 0], axis=2).any(axis=1)
+    cols = np.any(image != [0, 0, 0], axis=2).any(axis=0)
+
+    image_without_borders = image[rows][:, cols, :]
+
+    return image_without_borders
