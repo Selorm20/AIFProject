3152796f4119269afb43fc17afde94e318192c79
Selorm20 <selormapoh61@gmail.com>
2023-12-29 17:03:49 +0100

    update

--- SelormWorkSpace/MovingLibrary.py
+++ SelormWorkSpace/MovingLibrary.py
@@ -2,10 +2,41 @@ class Moving:
     def __init__(self, env):
         self.__env = env
         
+    
     #Do next position to enviroment
-    #return True if move is done
-    #return False if move is not done after call this method
+    
     def Move(self, ActualPosition, NextPosition):
-        return True
+        Moves = {
+            0: (-1, 0), #moveUp
+            1: (1, 0),  #moveDown
+            2: (0, -1), #moveLeft
+            3: (0, 1)   #moveRight
+        } 
+
+        if NextPosition in Moves:
+            coordinates = Moves[NextPosition]
+            new_position = (ActualPosition[0] + coordinates[0], ActualPosition[1] + coordinates[1])
+            
+            #return True if move is done
+            if self.move_is_valid(new_position):
+                ActualPosition = new_position
+                print("Moved to {ActualPosition}")
+                return True
+            
+            #return False if move is not done after call this method
+            else:
+                print("Move invalid. Try another")
+            
+            return False
+        
+        #Defining the move_valid variable to make sure agent stays in environment
+        def move_is_valid(self, new_position):
+            return 0 <= new_position[0] < len(self.__env) and 0 <= new_position[1] < len(self.__env[0])
+        
+        
+    
+
+
+
     
     
